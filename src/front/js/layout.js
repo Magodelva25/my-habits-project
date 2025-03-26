c4b6d27a129547e5a75b7be3426fa3d70366b43c
Mangodev <migudz@gmail.com>
2025-03-26 17:57:45 +0000

    darkmode

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BackendURL } from "./component/backendURL";
 
@@ -19,20 +19,9 @@ const Layout = () => {
     //the basename is used when your project is published in a subdirectory and not in the root of the domain
     // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
-    const [darkMode, setDarkMode] = useState(false);
 
     if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
 
-    useEffect(() => {
-        const getItem = localStorage.getItem("selectedTheme") || "light"; // Por defecto a "light"
-        if (getItem === "dark") {
-          document.documentElement.setAttribute('data-theme', 'dark');
-          setDarkMode(true);
-        } else {
-          document.documentElement.setAttribute('data-theme', 'light');
-          setDarkMode(false);
-        }
-      }, []);
 
     return (
         <div className="main-container">
